8a516e0fedd5407a6b4dcef323f01f8240c47825
Sean Meng <sean_meng@berkeley.edu>
2019-03-30 05:25:57 -0700

    added test to data.py

---
+++ src/data-test.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.insert(0, '../src')
+
+import data
+
+KEY_POINTS = [
+            "Nose",
+            "Neck",
+            "RShoulder",
+            "RElbow",
+            "RWrist",
+            "LShoulder",
+            "LElbow",
+            "LWrist",
+            "MidHip",
+            "RHip",
+            "RKnee",
+            "RAnkle",
+            "LHip",
+            "LKnee",
+            "LAnkle",
+            "REye",
+            "LEye",
+            "REar",
+            "LEar",
+            "LBigToe",
+            "LSmallToe",
+            "LHeel",
+            "RBigToe",
+            "RSmallToe",
+            "RHeel"
+        ]
+
+s = data.get_frames("live", KEY_POINTS)
+print(s)
