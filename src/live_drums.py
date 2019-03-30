7054cfbc621d94b5ce0691e69288eb6571184485
Isaiah Nields <imnields@gmail.com>
2019-03-30 12:37:23 -0700

    Add  Data

--- src/live_drums.py
+++ src/live_drums.py
@@ -5,7 +5,7 @@ import traceback
 import pygame
 from os import listdir
 
-folder = 'guitar'
+folder = 'drums'
 
 # initialize pygame
 pygame.mixer.init()
@@ -54,7 +54,7 @@ KEY_POINTS = [
 
 # try to erase the previous data
 try: rmtree('./../data/live')
-except Exception as e: print(e)
+except Exception as e: pass
 
 # initialize count and debounce
 count = 0
@@ -62,6 +62,15 @@ debounce = 500
 
 # create a list for right wrist points
 RWrists = []
+RShoulders = []
+
+LWrists = []
+LShoulders = []
+
+rw_idx = KEY_POINTS.index('RWrist')
+rs_idx = KEY_POINTS.index('RShoulder')
+lw_idx = KEY_POINTS.index('LWrist')
+ls_idx = KEY_POINTS.index('LShoulder')
 
 # continuously search for new json files
 while True:
@@ -73,56 +82,13 @@ while True:
             people = json.load(f)['people']
 
             # get the pose key points
-            data = people[0]['pose_keypoints_2d'] if people else [0 for _ in range(75)]
+            data = people[0]['pose_keypoints_2d'] if people else [0 for _ in range(78)]
 
-            # get the indices for left hip and wrist and right wrist
-            lh_idx, lw_idx, rw_idx = KEY_POINTS.index('LHip'), KEY_POINTS.index('LWrist'), KEY_POINTS.index('RWrist')
+            RWrists.append(np.array(data[rw_idx * 3: rw_idx * 3 + 1]))
+            RShoulders.append(np.array(data[rw_idx * 3: rw_idx * 3 + 1]))
 
-            # get the current left hip and left wrist points
-            LHip = np.array(data[lh_idx * 3: lh_idx * 3 + 2])
-            LWrist = np.array(data[lw_idx * 3: lw_idx * 3 + 2])
 
-            # get the current right wrist
-            RWrists.append(np.array(data[rw_idx * 3: rw_idx * 3 + 2]))
-
-            # if the length of wrists is more than 5
-            if len(RWrists) > 5:
-
-                # remove the first element
-                RWrists.pop(0)
-
-            # calculate the wrist velocity for the last half second
-            RWristVelocity = np.array([0.0, 0.0]) if len(RWrists) < 5 else (RWrists[-1] - RWrists[0]) / 0.5
-
-            # calculate the left wrist difference from the hip
-            handDistance = np.linalg.norm(LHip - LWrist) / 1
-
-            # if the right wrist's downward velocity is more than 0.2
-            if RWristVelocity[1] > 0.2 and debounce < 0 and handDistance > 0.05:
-
-                i_prev = 0.05
-
-                # iterate over the sounds
-                for sound, i in zip(sounds, np.linspace(0.2, 0.8, len(sounds))):
-
-                    # if the hand distance is in the right range
-                    if i_prev < handDistance < i:
-
-                        # set the sound volume based on strum speed
-                        sound.set_volume(np.linalg.norm(RWristVelocity))
-
-                        # play the sound
-                        pygame.mixer.Sound.play(sound)
-
-                # print the strum
-                print('Strum:', handDistance)
-
-                # reset the debounce
-                debounce = 500
-
-            # increment count and decrement debounce
-            debounce -= 1
-            count += 1
+        count += 1
 
     except FileNotFoundError:
         if count > 0:
