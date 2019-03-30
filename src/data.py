f8567de0fcc1f6128b3ff04f1b2e521ebd6b5295
Sean Meng <sean_meng@berkeley.edu>
2019-03-30 05:23:38 -0700

    resolved bug with data.py, jupyter notebook compatibility still questionable

--- src/data.py
+++ src/data.py
@@ -26,32 +26,35 @@ KEY_POINTS = [
             "LHeel",
             "RBigToe",
             "RSmallToe",
-            "RHeel",
-            "Background"
+            "RHeel"
         ]
 
 
-def get_frames(video, key_points):
+def get_frames(video, key_points = KEY_POINTS):
 
     # initialize the result array
     result = {key_point: [] for key_point in key_points}
 
+    data_dir = './data/'
+
     # iterate over each file in the folder
-    for file in listdir('./../data/' + video):
+    for file in listdir(data_dir + video):
 
         # open the video data
-        with open('./../data/' + video + '/' + file) as f:
-
+        with open(data_dir + video + '/' + file) as f:
             # load the json in to a dictionary
-            data = json.load(f)['people'][0]['pose_keypoints_2d']
+            try:
+                data = json.load(f)['people'][0]['pose_keypoints_2d']
+            except IndexError:
+                continue
 
-            # iterate over the key_points
-            for key_point in key_points:
+            if len(data) == 3*len(KEY_POINTS):
+                # iterate over the key_points
+                for key_point in key_points:
+                    # get the index of the key point
+                    idx = KEY_POINTS.index(key_point)
 
-                # get the index of the key point
-                idx = KEY_POINTS.index(key_point)
-
-                # extract the coordinate
-                result[key_point].append((data[idx * 3], data[idx * 3]))
+                    # extract the coordinate
+                    result[key_point].append((data[idx * 3], data[idx * 3 + 1]))
 
     return result
